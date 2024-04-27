0e793dfa4a03e4a4a04bab28b6e1f40c325b011b
DEEPAKBOBBY <130783950+DEEPAKBOBBY@users.noreply.github.com>
2024-04-27 17:09:52 +0530

    Add files via upload

---
+++ templates/usingpandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+ef=pd.read_csv("C:\\Users\\kalyan king\\PycharmProjects\\Medico project\\medicinedatasets\\Medicine_Details.csv")
+
+new_data = {
+            'Medicine Name': 'hi',
+            'Composition':'hlo',
+            'Uses': 'loo',
+            'Side_effects': 'ppp',
+            'Image URL': 'oooo',
+            'Manufacturer':'popopop'
+        }
+ef=ef._append(new_data,ignore_index=True)
+kk=ef.to_csv("C:\\Users\\kalyan king\\PycharmProjects\\Medico project\\medicinedatasets\\Medicine_Details.csv",index=False)
+if kk:
+    print("no")
+else:
+    print("yes")
+
+
